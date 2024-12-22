8201b0ab9e240ebc130199030a210f7c2ff89e83
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-22 10:35:57 +0300

    refactor: remove unused window icon from homepage

--- src/app/page.js
+++ src/app/page.js
@@ -28,13 +28,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+
           Examples
         </a>
         <a
