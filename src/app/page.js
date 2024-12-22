713bd5eb505154c34b52b650e93698d0dbf81c46
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-22 10:36:30 +0300

    refactor: remove examples link from homepage

--- src/app/page.js
+++ src/app/page.js
@@ -16,15 +16,7 @@ export default function Home() {
 
           Learn
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Examples
-        </a>
+      
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
