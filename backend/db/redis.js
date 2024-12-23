7052d2b3f91d7be9e5632a6c9fa8bc17d3693370
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-23 12:22:50 +0300

    feat: export Redis client and set initial key-value pair

--- backend/db/redis.js
+++ backend/db/redis.js
@@ -2,5 +2,5 @@ import Redis from "ioredis"
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = new Redis(process.env.REDIS_URL);
-await client.set('foo', 'bar');
+export const redis = new Redis(process.env.REDIS_URL);
+ await redis.set('foo', 'bar');
