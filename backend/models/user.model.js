f5af70ef5ded9fabe5d3bd5b364bf2d082192332
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-23 11:17:27 +0300

    feat: add password hashing to user model before saving

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -35,6 +36,20 @@ const userSchema = new mongoose.Schema({
         default: "customer",
     }
 },{timestamps: true});
+
+
+// hash the password before saving it to the database
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password"))return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 // create a model using the schema
 const User = mongoose.model("User", userSchema);
