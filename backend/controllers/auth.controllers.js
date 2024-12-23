95b4994d0c7b1e24c402b228108e1a1b0db46d42
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-23 10:30:55 +0300

    feat: implement authentication controllers and routes for signup, login, and logout

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const signup = async (req, res) => {
+    console.log("signup controller");
+}
+
+export const login = async (req, res) => {
+    console.log("login controller");
+}
+
+export const logout = async (req, res) => {
+    console.log("logout controller");
+}
+
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,13 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
 
+router.post("/login", login);
+
+router.get("/logout", logout);
 
 // exporting the router
 export default router;
