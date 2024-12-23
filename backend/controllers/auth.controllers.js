095c1b389cc01b2426c23030f5eb223b22bc3667
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-23 13:12:33 +0300

    feat: refactor token generation and signup process for improved validation and error handling

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -5,80 +5,79 @@ import { redis }  from "../db/redis.js";
 
 
 
+const generateTokens = (userId) => {
+	const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "15m",
+	});
 
-const generateToken = async (userId) => {
-    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-    });
-    const refreshToken = jwt.sign({ userId }, process.env.JWT_SECRET_REFRESH, {
-        expiresIn: "7d",
-    });
-    return { accessToken, refreshToken };
-}
+	const refreshToken = jwt.sign({ userId }, process.env.JWT_SECRET_REFRESH, {
+		expiresIn: "7d",
+	});
+
+	return { accessToken, refreshToken };
+};
 
 const storeRefreshToken = async (userId, refreshToken) => {
-    await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7 days
-    return true; 
-}
+	await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); // 7days
+};
 
 const setCookies = (res, accessToken, refreshToken) => {
-    res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 15 * 60 * 1000, // 7 days
-      });
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 15 * 60 * 1000, // 7 days
-      });
-}
+	res.cookie("accessToken", accessToken, {
+		httpOnly: true, 
+		sameSite: "strict", 
+		maxAge: 15 * 60 * 1000, // 15 minutes
+	});
+	res.cookie("refreshToken", refreshToken, {
+		httpOnly: true, 
+		sameSite: "strict", 
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
+};
 
 export const signup = async (req, res) => {
-    const { name, email, password } = req.body;
-    
-    if (!name || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
+	const { email, password, name } = req.body;
+	try {
+		const userExists = await User.findOne({ email });
 
-    if (!email.includes("@")) {
-        return res.status(400).json({ message: "Please add a valid email address" });
-    }
+		if (userExists) {
+			return res.status(400).json({ message: "User already exists" });
+		}
 
-    if (password.length < 8) {
-        return res.status(400).json({ message: "Password must be at least 8 characters long" });
-    }
-
-    // check if user already exists
-    const user = await User.findOne({ email });
-    if (user) {
-        return res.status(400).json({ message: "User already exists" });
-    }
-
-    try {
-      
-        // authenticate the user
-        const {accessToken, refreshToken} = await generateToken(user._id);
-        await storeRefreshToken(user._id, refreshToken);
-
-        setCookies(res, accessToken, refreshToken);
-
-        // hash the password
+        if (!email.includes("@")) {
+			return res.status(400).json({ message: "Please add a valid email address" });
+		}
+        if(!name || !password || !email){
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        // hash password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // create a new user
-        const user = User.create({ name, email, password : hashedPassword });
-        res.status(201).json({ message: "User created successfully", user :{
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        } });
 
-    } catch (error) {
-        return res.status(500).json({ message: "Something went wrong" });
-    }
-}
+		const user = await User.create({ name, email, password : hashedPassword });
+
+		// authenticate
+		const { accessToken, refreshToken } = generateTokens(user._id);
+		await storeRefreshToken(user._id, refreshToken);
+
+		setCookies(res, accessToken, refreshToken);
+
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+            message: "User created successfully",
+            success: true,
+		});
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ 
+            message: error.message 
+        });
+        return;
+	}
+};
 
 export const login = async (req, res) => {
     console.log("login controller");
