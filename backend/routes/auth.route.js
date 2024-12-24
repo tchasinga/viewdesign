e60569300390f5c4e59078e981e76ae4c3537a2b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 09:26:58 +0300

    feat: add authentication middleware for protected routes and update profile route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,9 +2,11 @@ import express from "express";
 import {
   login,
   logout,
+  profile,
   refreshToken,
   signup,
 } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -12,7 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-router.get("/profile", protectedRouter, profile);
+router.get("/profile", protectRoute, profile);
 
 // exporting the router
 export default router;
--- backend/routes/productsroutes.route.js
+++ backend/routes/productsroutes.route.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { getAllProducts } from "../controllers/products.controllers.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 // initialize express and other port app
 const router = express.Router();
 
-router.get("/",protectedRouter, adminRoute, getAllProducts);
-router.get("/:id", getProductById);
-router.post("/", createProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.get("/",protectRoute, adminRoute, getAllProducts);
+// router.get("/:id", getProductById);
+// router.post("/", createProduct);
+// router.put("/:id", updateProduct);
+// router.delete("/:id", deleteProduct);
 
 
 export default router;
