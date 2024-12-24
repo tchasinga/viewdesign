48742dbee748f71cea7234001621359cfa4720dd
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 11:43:29 +0300

    feat: add getCartProduct function to retrieve cart items with quantities

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { addToCart, getCart, removeAllFromCart, updateQuantity } from "../controllers/cart.controllers.js";
+import { addToCart, getCartProduct, removeAllFromCart,   } from "../controllers/cart.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // route to add to cart
 router.post("/add", protectRoute, addToCart);
-router.get("/get", protectRoute, getCart);
+router.get("/get", protectRoute, getCartProduct);
 router.delete("/remove", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
 
