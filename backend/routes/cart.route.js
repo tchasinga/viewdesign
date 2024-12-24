ac49119bacf6717ba4efcbcbbd0059514b51bb09
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 11:27:29 +0300

    feat: add route to remove all items from cart

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { addToCart, getCart, removeFromCart } from "../controllers/cart.controllers.js";
+import { addToCart, getCart, removeAllFromCart } from "../controllers/cart.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+
+// route to add to cart
+router.post("/add", protectRoute, addToCart);
+router.get("/get", protectRoute, getCart);
+router.delete("/remove", protectRoute, removeAllFromCart);
 
 
 // exporting the router
