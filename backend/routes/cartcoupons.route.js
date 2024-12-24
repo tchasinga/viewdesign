88133b2ed4873beb2bffc18e3703cfa29d9722c8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 12:07:18 +0300

    feat: implement getCoupon controller and add route for fetching active coupons

--- backend/routes/cartcoupons.route.js
+++ backend/routes/cartcoupons.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getCoupon } from "../controllers/coupon.controllers.js";
 
 const router = express.Router();
 
-
+router.get("/", protectRoute, getCoupon)
+router.post("/validate", protectRoute, validateCoupon)
 
 
 // exporting the router
