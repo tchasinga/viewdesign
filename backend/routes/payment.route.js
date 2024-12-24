de6da43dbae208c6fee6b320b4008e4a41fc01b0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 12:35:25 +0300

    feat: rename payment controller file and update import path

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller.js";
+import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controllers.js";
 
 const router = express.Router();
 
