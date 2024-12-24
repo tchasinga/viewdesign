181a2787e0a25a11cd7931d37ded4a67ab52b2e8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 09:02:10 +0300

    style: fix missing newline at end of product model file

--- backend/models/products.model.js
+++ backend/models/products.model.js
@@ -25,7 +25,7 @@ const productSchema = new mongoose.Schema({
     isFeatured: {
         type: Boolean,
         default: false,
-    },
+    }
 },{timestamps: true});
 const Product = mongoose.model("Product", productSchema);
 export default Product;
