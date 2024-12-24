6e1cd8f24a78276b2a03ed7cf337d84668950a14
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 10:56:24 +0300

    feat: implement getProductByIdCategory controller to retrieve products by category

--- backend/controllers/products.controllers.js
+++ backend/controllers/products.controllers.js
@@ -142,4 +142,13 @@ export const getRecommendedProducts = async (req, res) => {
 } 
 
 
-export const getProductByIdCategory = async (req, res) => {}
+export const getProductByIdCategory = async (req, res) => {
+    const { category } = req.params;
+	try {
+		const products = await Product.find({ category });
+		res.json({ products });
+	} catch (error) {
+		console.log("Error in getProductsByCategory controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
