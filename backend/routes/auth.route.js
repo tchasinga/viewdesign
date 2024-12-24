1f6076c2a65b02f15207e941cd289f92fa88bcd9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 08:40:23 +0300

    feat: add refresh token route to authentication API

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import { login, logout, refreshToken, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.post("/refresh-token", refreshToken);
 
 // exporting the router
 export default router;
