0d69bf6ffe57d68ec1d01f6a96e3dfae07918549
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 10:39:43 +0300

    feat: implement getRecommendedProducts controller and update route for recommended products

--- backend/controllers/products.controllers.js
+++ backend/controllers/products.controllers.js
@@ -117,4 +117,26 @@ export const deleteProduct = async (req, res) => {
 	}
 }
 
-export const getRecommendedProducts = async (req, res) => {}
+export const getRecommendedProducts = async (req, res) => {
+    try {
+		const products = await Product.aggregate([
+			{
+				$sample: { size: 4 },
+			},
+			{
+				$project: {
+					_id: 1,
+					name: 1,
+					description: 1,
+					image: 1,
+					price: 1,
+				},
+			},
+		]);
+
+		res.json(products);
+	} catch (error) {
+		console.log("Error in getRecommendedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+} 
--- backend/routes/productsroutes.route.js
+++ backend/routes/productsroutes.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controllers/products.controllers.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getRecommendedProducts } from "../controllers/products.controllers.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 // initialize express and other port app
@@ -7,9 +7,7 @@ const router = express.Router();
 
 router.get("/",protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.get("/recommended",protectRoute, getRecommendedProducts);
-
-// I'll add the free vesion of get recommended products later
+router.get("/recommended", getRecommendedProducts);
 
 router.post("/createproductasadmin", protectRoute, adminRoute, createProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
