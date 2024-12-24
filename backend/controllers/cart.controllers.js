8a184bafacf6c24c8848b1d9ab909112178f9d0b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 11:37:53 +0300

    feat: implement addToCart and removeAllFromCart functions in cart controller

--- backend/controllers/cart.controllers.js
+++ backend/controllers/cart.controllers.js
@@ -1,4 +1,38 @@
+import Product from "../models/products.model.js";
 
 
+export const addToCart = async (req, res) => {
+    try {
+		const { productId } = req.body;
+		const user = req.user;
 
-export const addToCart = async (req, res) => {}
+		const existingItem = user.cartItems.find((item) => item.id === productId);
+		if (existingItem) {
+			existingItem.quantity += 1;
+		} else {
+			user.cartItems.push(productId);
+		}
+
+		await user.save();
+		res.json(user.cartItems);
+	} catch (error) {
+		console.log("Error in addToCart controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
+
+export const removeAllFromCart = async (req, res) => {
+    try {
+		const { productId } = req.body;
+		const user = req.user;
+		if (!productId) {
+			user.cartItems = [];
+		} else {
+			user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+		}
+		await user.save();
+		res.json(user.cartItems);
+	} catch (error) {
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
