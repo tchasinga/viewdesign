01b7234d9c4c8f2e549e75452e019bb673aecb10
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 12:09:47 +0300

    feat: add validateCoupon import to cartcoupons route for enhanced functionality

--- backend/routes/cartcoupons.route.js
+++ backend/routes/cartcoupons.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getCoupon } from "../controllers/coupon.controllers.js";
+import { getCoupon, validateCoupon } from "../controllers/coupon.controllers.js";
 
 const router = express.Router();
 
