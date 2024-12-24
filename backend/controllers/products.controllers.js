c2f18c583bdb0eed368c829a86afbb96a7cb6858
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-24 09:49:47 +0300

    feat: implement getFeaturedProducts controller with Redis caching

--- backend/controllers/products.controllers.js
+++ backend/controllers/products.controllers.js
@@ -1,5 +1,5 @@
 import Product from "../models/products.model.js";
-
+import {redis} from "../db/redis.js";
 
 
 
@@ -17,6 +17,38 @@ export const getAllProducts = async (req, res) => {
         }
     } catch (error) {
         console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({ 
+            message: error.message,
+            error: error.message,
+            success : false
+        });
+    }
+}
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+
+        let featuredProducts = await redis.get("featured_products");
+		if (featuredProducts) {
+			return res.json(JSON.parse(featuredProducts));
+		}
+
+        const products = await Product.find({ isFeatured: true }).lean();
+        res.json(products);
+
+        if (products.length === 0) {
+            res.status(404).json({ message: "No featured products found" });
+        }
+
+        if (products.length > 0) {
+            res.status(200).json({ message: "Featured products found", products });
+        }
+
+        // store in redis for future quick access
+
+		await redis.set("featured_products", JSON.stringify(featuredProducts));
+    } catch (error) {
+        console.log("Error in getFeaturedProducts controller", error.message);
         res.status(500).json({ 
             message: error.message,
             error: error.message,
