9e41bfba7460322431bd07042f06d74a947931e4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-12-25 09:51:49 +0300

    feat: update routing paths for login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Homepages />} />
-          <Route path='/about' element={<Loginpages />} />
-          <Route path='/contact' element={<Singuppages />} />
+          <Route path='/login' element={<Loginpages />} />
+          <Route path='/singup' element={<Singuppages />} />
         </Routes>
       </BrowserRouter>
     </div>
