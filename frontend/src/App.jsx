0bde822ff92e2f3a20b332753956e4639bc11581
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-01 09:50:30 +0300

    fix: correct typo in background gradient comment in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
         <div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
       </div>
     </div>
-    {/* Background gradient is added here for making thn background image  */}
+    {/* Background gradient is added here for making thn background  */}
      <div className="relative z-50 pt-20">
       <BrowserRouter>
         <Navbar /> 
